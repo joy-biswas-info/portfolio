3f939823654017c68b42919e742b7ac36453984f
Joy Biswas <contact.joybiswas@gmail.com>
2025-06-16 23:49:22 +0600

    [Done]Portfolio Website

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+// import Hero from "./components/hero/Hero";
+// import Services from "./components/services/Services";
+// import Portfolio from "./components/portfolio/Portfolio";
+// import Contact from "./components/contact/Contact";
+
+import { lazy, Suspense } from "react";
+import LazyLoad from "react-lazyload";
+import Navbar from "./components/navbar/Navbar.jsx";
+
+const Hero = lazy(() => import("./components/hero/Hero"));
+const Services = lazy(() => import("./components/services/Services"));
+const Portfolio = lazy(() => import("./components/portfolio/Portfolio"));
+const Contact = lazy(() => import("./components/contact/Contact"));
+
+const App = () => {
+  return (
+    <div className="container">
+      <Suspense fallback={"loading..."}>
+        <LazyLoad height={"100vh"} offset={100}>
+          <section id="Homeage">
+            {/* <Navbar /> */}
+            <Navbar />
+            <Hero />
+          </section>
+        </LazyLoad>
+      </Suspense>
+      <Suspense fallback={"loading..."}>
+        <LazyLoad height={"100vh"} offset={-100}>
+          <section id="Services">
+            <Services />
+          </section>{" "}
+        </LazyLoad>
+      </Suspense>
+      <Suspense fallback={"loading..."}>
+        <LazyLoad height={"600vh"} offset={-100}>
+          <section id="Portfolio">
+            <Portfolio />
+          </section>{" "}
+        </LazyLoad>
+      </Suspense>
+      <Suspense fallback={"loading..."}>
+        <LazyLoad height={"100vh"} offset={-100}>
+          <section id="Contact">
+            <Contact />
+          </section>{" "}
+        </LazyLoad>
+      </Suspense>
+    </div>
+  );
+};
+
+export default App;
